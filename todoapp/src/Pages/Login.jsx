ca4d0e47b41cff555b9214fe502e17301f447a62
sumansauravmay <sumansauravmay12345@gmail.com>
2024-10-02 18:31:38 +0530

    signup

--- todoapp/src/Pages/Login.jsx
+++ todoapp/src/Pages/Login.jsx
@@ -3,33 +3,33 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <h1 class="text-center mt-2">Login</h1>
-      <div class="d-flex justify-content-center">
-        <form class="w-25 border border-dark p-5 mt-5">
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+      <h1 className="text-center mt-2">Login</h1>
+      <div className="d-flex justify-content-center">
+        <form className="w-25 border border-dark p-5 mt-5">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
           </div>
 
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
             />
           </div>
 
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Login
           </button>
         </form>
--- todoapp/src/Pages/Signup.jsx
+++ todoapp/src/Pages/Signup.jsx
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  let [email, setEmail] = useState("");
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    let data = { email, username, password };
+    axios
+      .post(`https://todo-app-full-stack-f52j.onrender.com/todo/register`, data)
+      .then((res) => {
+        console.log("signup data", res.data.user);
+      })
+      .catch((err) => console.log("err", err));
+  };
+
   return (
     <>
-      <h1 class="text-center mt-2">Signup</h1>
-      <div class="d-flex justify-content-center">
-        <form class="w-25 border border-dark p-5 mt-5">
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+      <h1 className="text-center mt-2">Signup</h1>
+      <div className="d-flex justify-content-center">
+        <form className="w-25 border border-dark p-5 mt-5">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Username
             </label>
-            <input type="text" class="form-control" id="exampleInputUsername" />
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputUsername"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <button type="submit" class="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSignup}
+          >
             Sign Up
           </button>
         </form>
